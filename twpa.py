b7b0c96278581bba9cb7db2e8674295c9d2bbf03
dkurchigin <kurchigin.dmitry@yandex.ru>
2019-01-12 16:57:27 +0300

    find uniq words & sort

--- twpa.py
+++ twpa.py
@@ -4,8 +4,9 @@ import os
 directory = '.'
 files = os.listdir(directory)
 
-rule_list = []
+word_list = []
 
+WORD_WEIGHT = 3
 
 def print_files_on_dir(text):
     n = 0   
@@ -30,27 +31,28 @@ def copy_rules_from_file(file_path):
     f = open(file_path, 'r', encoding='utf8')
     
     for line in f:
-        pattern_for_rules = re.findall(r'.*',line)
-        pattern_for_rules2 = "".join(pattern_for_rules)
-        utterance = pattern_for_rules2.split()
+        pattern_re = re.findall(r'.*',line)
+        pattern_for_utterance = "".join(pattern_re)
+        utterance = pattern_for_utterance.split()
         for word in utterance:
-            if len(word) >= 3:
-                print (word)
-
-        n = 0
-        equal_file = False
-        while n < len(rule_list):
-            if rule_list[n] == pattern_for_rules:
-                equal_file = True
-            n += 1
-        if equal_file == False:
-            rule_list.append(pattern_for_rules)
-        else:
-            equal_file = False
+            if len(word) >= WORD_WEIGHT:
+                n = 0
+                equal_file = False
+                while n < len(word_list):
+                    if word_list[n] == word:
+                        equal_file = True
+                    n += 1
+                if equal_file == False:
+                    word_list.append(word)
+                else:
+                    equal_file = False
 
     f.close()
-    n = 0
+    
 
 print_files_on_dir("CSV") #files in dir
 file_number = int(input())
 copy_rules_from_file(str(files[file_number]))
+
+word_list = sorted(word_list)
+print (word_list)
