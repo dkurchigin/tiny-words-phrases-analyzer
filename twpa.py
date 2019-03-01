769cd503a2e93750cd40560934b0bef95ec5907e
dkurchigin <kurchigin.dmitry@yandex.ru>
2019-03-02 00:14:00 +0300

    have fast sqlite now

--- twpa.py
+++ twpa.py
@@ -9,6 +9,7 @@ word_list = []
 
 WORD_WEIGHT = 3
 
+
 def check_db_exist(file_path):
     print(file_path)
     base_extension = ".db"
@@ -19,13 +20,17 @@ def check_db_exist(file_path):
     create_db(file_path)
     print("Create database:", file_path)
 
-def write_data(file_path, word, line_in_file):
-    con = sqlite3.connect(file_path)
 
+def write_data(file_path, list_sql):
+    con = sqlite3.connect(file_path)
     cur = con.cursor()
-    cur.execute('INSERT INTO parsed_file (id, word, line_in_file) VALUES(NULL, "' + word + '", "' + str(line_in_file) + '")')
+    for element in list_sql:
+        #print(type(element), element)
+        word = re.sub(r'\s.*$', '', element)
+        line_in_file = re.sub(r'^.*\s', '', element)
+        cur.execute('INSERT INTO parsed_file (id, word, line_in_file) VALUES(NULL, "' + word + '", "' + str(line_in_file) + '")')
     con.commit()
-    #print(cur.lastrowid)
+    con.close()
 
 def show_data_in_db(file_path):
     con = sqlite3.connect(file_path)
@@ -35,34 +40,36 @@ def show_data_in_db(file_path):
     print(cur.fetchall())
     con.close()
 
+
 def create_db(file_path):
     con = sqlite3.connect(file_path)
 
     cur = con.cursor()
     cur.execute('CREATE TABLE parsed_file (id INTEGER PRIMARY KEY, word VARCHAR(100), line_in_file VARCHAR(30))')
+    cur.execute('CREATE TABLE words (id INTEGER PRIMARY KEY, word VARCHAR(100), count VARCHAR(30))')
     con.commit()
     con.close()
 
-    #write_data(file_path)
 
 def print_files_on_dir(text):
     n = 0   
-    print ('')
-    print ('')
-    print ('===List of files===')
-    print ('')
-    print ('')
+    print('')
+    print('')
+    print('===List of files===')
+    print('')
+    print('')
     while n < len(files):
         name_of_file = files[n]
         result_string = "[" + str(n) + "]" + " - " + name_of_file
         print (result_string)
         n += 1
-    print ('')
-    print ('')
-    print ('===================')
-    print ('')
+    print('')
+    print('')
+    print('===================')
+    print('')
     final_message = "Select " + text + " with tests. Enter the number:"
-    print (final_message)
+    print(final_message)
+
 
 def copy_rules_from_file(file_path):
     f = open(file_path, 'r', encoding='utf8')
@@ -81,37 +88,36 @@ def copy_rules_from_file(file_path):
                     n += 1
                 if not equal_file:
                     word_list.append(word)
-
     f.close()
+
 
 def calc_words_count(file_path):
     global word_list
+    list_for_sql = []
     word_list = sorted(word_list)
-    i = 0
-    words_count = {}
-    #init words_count
-    for element in word_list:
-        words_count[word_list[i]] = 1
-        i += 1
 
     #work with file again
     i = 0
-    while i < len(words_count):
-        scan_file_again(i,file_path)
+    while i < len(word_list):
+        out_sql_element = scan_file_again(i,file_path)
+        for element in out_sql_element:
+            list_for_sql.append(element)
         i += 1
+    write_data(file_path + ".db", list_for_sql)
 
-def scan_file_again(m,file_path):
+
+def scan_file_again(m, file_path):
     f = open(file_path, 'r', encoding='utf8')
     line_number = 0
+    word_in_lines = []
     for line in f:
         line_number += 1
         target_line = re.findall(word_list[m], line)
         if target_line:
-            #print (target_line)
-            #print("Word:",word_list[m], " - number in line", len(target_line), "number of line", line_number)
-            #print(file_path + ".db")
-            write_data(file_path + ".db", word_list[m], line_number)
+            summing_str = word_list[m] + " " + str(line_number)
+            word_in_lines.append(summing_str)
     f.close()
+    return word_in_lines
 
 
 print_files_on_dir("CSV") #files in dir
