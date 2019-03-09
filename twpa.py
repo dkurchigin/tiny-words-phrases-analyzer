08f408c4183e4c052374487bf03fcbcca70ac062
dkurchigin <kurchigin.dmitry@yandex.ru>
2019-03-10 00:44:53 +0300

    rework code + add new table + func for find phrases by keywords

--- twpa.py
+++ twpa.py
@@ -14,12 +14,14 @@ def check_db_exist(file_path):
     print(file_path)
     base_extension = ".db"
     file_path = file_path + base_extension
+
     if os.path.isfile(file_path):
         os.remove(file_path)
         print("Delete older version of", file_path)
     create_db(file_path)
     print("Create database:", file_path)
 
+    return file_path
 
 def write_data(file_path, list_sql, table):
     con = sqlite3.connect(file_path)
@@ -32,6 +34,8 @@ def write_data(file_path, list_sql, table):
         elif table == 'phrases':
             element = re.sub(r'\n', '', element)
             cur.execute('INSERT INTO phrases (id, phrase) VALUES(NULL, "' + element + '")')
+        elif table == 'unique_words':
+            cur.execute('INSERT INTO unique_words (id, word) VALUES(NULL, "' + element + '")')
     con.commit()
     con.close()
 
@@ -42,6 +46,8 @@ def show_data_in_db(file_path, table):
     cur = con.cursor()
     cur.execute('SELECT * FROM ' + table)
     print(cur.fetchall())
+    #for element in cur.fetchall():
+        #print(element[1])
     con.close()
 
 
@@ -51,26 +57,16 @@ def create_db(file_path):
     cur = con.cursor()
     cur.execute('CREATE TABLE parsed_file (id INTEGER PRIMARY KEY, word VARCHAR(100), line_in_file VARCHAR(30))')
     cur.execute('CREATE TABLE phrases (id INTEGER PRIMARY KEY, phrase VARCHAR(255))')
+    cur.execute('CREATE TABLE unique_words (id INTEGER PRIMARY KEY, word VARCHAR(255))')
     con.commit()
     con.close()
 
 
 def print_files_on_dir(text):
-    n = 0   
-    print('')
-    print('')
-    print('===List of files===')
-    print('')
-    print('')
-    while n < len(files):
-        name_of_file = files[n]
-        result_string = "[" + str(n) + "]" + " - " + name_of_file
-        print (result_string)
-        n += 1
-    print('')
-    print('')
-    print('===================')
-    print('')
+    print('\n===List of files===')
+    for file in files:
+        print("[{}] - {}".format(files.index(file), file))
+    print('===================\n')
     final_message = "Select " + text + " with tests. Enter the number:"
     print(final_message)
 
@@ -102,14 +98,13 @@ def calc_words_count(file_path):
     global word_list
     list_for_sql = []
     word_list = sorted(word_list)
+    write_data(file_path + ".db", word_list, "unique_words")
 
     #work with file again
-    i = 0
-    while i < len(word_list):
-        out_sql_element = scan_file_again(i,file_path)
+    for word in word_list:
+        out_sql_element = scan_file_again(word_list.index(word),file_path)
         for element in out_sql_element:
             list_for_sql.append(element)
-        i += 1
     write_data(file_path + ".db", list_for_sql, "parsed_file")
 
 
@@ -121,18 +116,42 @@ def scan_file_again(m, file_path):
         line_number += 1
         target_line = re.findall(word_list[m], line)
         if target_line:
-            summing_str = word_list[m] + " " + str(line_number)
-            word_in_lines.append(summing_str)
+            word_in_lines.append("{} {}".format(word_list[m], line_number))
     f.close()
     return word_in_lines
+
+
+def find_phrases_by_words(file_path, table, column, *words):
+    con = sqlite3.connect(file_path)
+
+    cur = con.cursor()
+    cur.execute('SELECT * FROM ' + table + ' WHERE ' + summing_words_for_find(column, words) + '')
+    print(cur.fetchall())
+    con.close()
+
+
+def summing_words_for_find(column, words):
+    print(words, "<-")
+    resulting_string = ""
+    for word in words[0]:
+        if resulting_string == "":
+            resulting_string = resulting_string + "{} like \"%{}%\"".format(column, word)
+        else:
+            resulting_string = resulting_string + " and {} like \"%{}%\"".format(column, word)
+    return resulting_string
 
 
 print_files_on_dir("CSV") #files in dir
 file_number = int(input())
 
-check_db_exist(str(files[file_number]))
-copy_rules_from_file(str(files[file_number]))
+csv_file_path = str(files[file_number])
+db_file_path = check_db_exist(csv_file_path)
 
-calc_words_count(str(files[file_number]))
-show_data_in_db(str(files[file_number]) + ".db", "parsed_file")
-show_data_in_db(str(files[file_number]) + ".db", "phrases")
+copy_rules_from_file(csv_file_path)
+
+calc_words_count(csv_file_path)
+show_data_in_db(db_file_path, "parsed_file")
+show_data_in_db(db_file_path, "phrases")
+show_data_in_db(db_file_path, "unique_words")
+words = ["самсунг", "телевизор"]
+find_phrases_by_words(db_file_path, "phrases", "phrase", words)
